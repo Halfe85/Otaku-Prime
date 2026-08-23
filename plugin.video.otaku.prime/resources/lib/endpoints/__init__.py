33d43824e5e73bb383f46db3360bf9e3598cc35f
Halfe85 <halfe85@gmail.com>
2026-08-24 00:08:55 +0200

    Initialize endpoint package

---
+++ plugin.video.otaku.prime/resources/lib/endpoints/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""HTTP/API endpoint helpers for Otaku Prime."""
+
+from resources.lib.endpoints.auth_service import AuthenticatorAPI, AuthenticatorAPIError
+
+__all__ = ["AuthenticatorAPI", "AuthenticatorAPIError"]
