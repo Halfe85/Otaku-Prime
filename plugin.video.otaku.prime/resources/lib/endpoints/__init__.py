d14d887f5b961f1ad80b78e83b5b78718bee6e11
Halfe85 <halfe85@gmail.com>
2026-08-27 09:22:09 +0200

    Remove unused endpoint package re-exports

--- plugin.video.otaku.prime/resources/lib/endpoints/__init__.py
+++ plugin.video.otaku.prime/resources/lib/endpoints/__init__.py
@@ -1,6 +1,2 @@
 # -*- coding: utf-8 -*-
 """HTTP/API endpoint helpers for Otaku Prime."""
-
-from resources.lib.endpoints.auth_service import AuthenticatorAPI, AuthenticatorAPIError
-
-__all__ = ["AuthenticatorAPI", "AuthenticatorAPIError"]
