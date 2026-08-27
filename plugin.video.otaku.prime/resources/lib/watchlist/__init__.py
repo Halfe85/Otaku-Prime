243dbb4f667d217f254bc29aa539e317ec12341e
Halfe85 <halfe85@gmail.com>
2026-08-27 09:21:57 +0200

    Remove unused watchlist package re-exports

--- plugin.video.otaku.prime/resources/lib/watchlist/__init__.py
+++ plugin.video.otaku.prime/resources/lib/watchlist/__init__.py
@@ -1,14 +1,2 @@
 # -*- coding: utf-8 -*-
 """Watchlist provider integrations for Otaku Prime."""
-
-from resources.lib.watchlist.anilist import (
-    AniListAuthError,
-    AniListAuthenticator,
-    AniListViewer,
-)
-
-__all__ = [
-    "AniListAuthError",
-    "AniListAuthenticator",
-    "AniListViewer",
-]
