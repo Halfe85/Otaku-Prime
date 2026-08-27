5094b8a23a7fa0fe5b06ad9d7f64f2c0b91c9ae2
Halfe85 <halfe85@gmail.com>
2026-08-27 09:21:32 +0200

    Remove obsolete AniList UI compatibility shim

--- plugin.video.otaku.prime/resources/lib/watchlist/anilist_ui.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Compatibility import for the file-backed AniList UI component."""
-
-from __future__ import annotations
-
-from resources.lib.ui import render_anilist_auth
-
-__all__ = ("render_anilist_auth",)
